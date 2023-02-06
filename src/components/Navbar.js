fb5c105a6a8c008ec1a0b3949034f26233e16c4c
Piyush Deshmukh <99667276+Piyush-Deshmukh@users.noreply.github.com>
2023-02-06 09:43:55 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ function NavbarComponent() {
   const handleShow = () => setShow(true);
 
   const checkout = async () => {
-    await fetch("http://localhost:4000/checkout", {
+    await fetch("https://ecommerce-store-backend.onrender.com/checkout", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
